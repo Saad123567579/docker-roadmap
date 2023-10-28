c060774280bf85327b32d90f6deebcdaad00ddfc
Muhammad Saad <msaad.bscs22seecs@seecs.edu.pk>
2023-10-28 23:41:57 +0500

    x

--- part-1/1-simple-app/index.js
+++ part-1/1-simple-app/index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
+//docker
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
